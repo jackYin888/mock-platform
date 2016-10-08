20c26df7afb26850b1686adfc81a4526d5b201f0
JackYin <yjk@JACK.local>
2016-10-08 18:17:18 +0800

    解决冲突

--- src/app/components/buttonList.js
+++ src/app/components/buttonList.js
@@ -1,21 +1,21 @@
 /*
  * @Author: yangfengchu
  * @Date:   2016-08-25 18:55:36
- * @Last Modified 2016-09-18
- * @Last Modified time: 2016-09-18 16:36:12
+ * @Last Modified 2016-10-08
+ * @Last Modified time: 2016-10-08 17:51:00
  */
 
 //按钮组
 
 'use strict';
 import React, { Component } from 'react'
-import { connect } from "react-redux"
+import { connect } from 'react-redux'
 import * as actionCreators from '../actions/index'
 import { bindActionCreators } from 'redux'
-import ajax from './ajax';
 import fetch from 'fetch'
+import ajax from './ajax';
 let { href } = window.location;
-const { editUrl, editMethod } = actionCreators;
+// const { editUrl, editMethod } = actionCreators;
 class ButtonList extends Component {
     constructor(props) {
         super(props);
@@ -29,59 +29,57 @@ class ButtonList extends Component {
         alert('保存成功')
         console.log(resp)
     }
-    submitHandler() {
-        const data = {
-            "apiName": "getUserData",
-            "name": "获取用户数据",
-            "method": "GET",
-            "note": "备注",
-            "company": "beisen",
-            "version": "v2",
-            "group": "tita",
+    submitHandler(data) {
+        let temp = {
+            'apiName': 'getUserData',
+            'name': '获取用户数据',
+            'method': 'GET',
+            'note': '备注',
+            'company': 'beisen',
+            'version': 'v2',
+            'group': 'tita',
 
-            "parem": {
-                "filter": "name"
+            'parem': {
+                'filter': 'name'
             },
-            "path": "/beisen/api/v2/tita/getUserData",
-            "schema": {
-                "type": "object",
-                "properties": {
-                    "user": {
-                        "type": "object",
-                        "properties": {
-                            "id": {
-                                "$ref": "#/definitions/positiveInt"
+            'path': '/beisen/api/v2/crack/getUserData',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'user': {
+                        'type': 'object',
+                        'properties': {
+
+                            'name': {
+                                'type': 'string',
+                                'faker': 'name.findName'
                             },
-                            "name": {
-                                "type": "string",
-                                "faker": "name.findName"
-                            },
-                            "email": {
-                                "type": "string",
-                                "format": "email",
-                                "faker": "internet.email"
+                            'email': {
+                                'type': 'string',
+                                'format': 'email',
+                                'faker': 'internet.email'
                             }
-                        },
-                        "required": ["id", "name", "email"]
-                    }
-                },
-                "required": ["user"],
-                "definitions": {
-                    "positiveInt": {
-                        "type": "integer",
-                        "minimum": 0,
-                        "exclusiveMinimum": true
+                        }
                     }
                 }
+
             }
         }
-
-        fetch('/createSchema', {
+        let tempData = JSON.stringify(temp)
+        fetch('http://localhost:3001/createSchema', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
             method: 'POST',
-            body: data
-        }).then(function (data) {
-            debugger
+            body: tempData
+        }).then(function(response) {
+            // handle HTTP response
+            console.log(response)
+        }, function(error) {
+            console.log(error)
+                // handle network error
         })
+        ajax.req('post', href, this.postCallback, this.postError);
     }
     render() {
         return (
@@ -90,8 +88,8 @@ class ButtonList extends Component {
                     <h4>配置编辑器</h4>
                 </div>
                 <div className="col-md-6">
-                    <strong>保存接口(第三步) ：</strong>
-                    <button onClick={this.submitHandler} type="button" className="btn btn-xs btn-raised btn-success">保存API</button>
+                    <strong>保存接口(第三步)：</strong>
+                <button onClick={this.submitHandler} type="button" className="btn btn-xs btn-raised btn-success">保存API</button>
                 </div>
             </div>
         );
