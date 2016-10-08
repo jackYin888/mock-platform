8422e63f4384cca5126f462336f0f35e63aa2eb8
yangfengchu <yangfengchu@beisen.com>
2016-10-08 14:46:59 +0800

    build:增加转换模块还原基础类型

--- src/app/components/converter.js
+++ src/app/components/converter.js
@@ -1,19 +1,64 @@
 /*
  * @Author: yangfengchu
  * @Date:   2016-09-16 23:08:41
- * @Last Modified 2016-09-27
- * @Last Modified time: 2016-09-27 14:18:56
+ * @Last Modified 2016-10-08
+ * @Last Modified time: 2016-10-08 14:02:57
  */
 
 'use strict';
-//json tree data to json schema
 
-class Converter {
-    constructor(props) {
-        super(props);
+
+var json = {
+    "address": {
+        "streetAddress": "21 2nd Street",
+        "city": "New York"
+    },
+    "phoneNumber": [{
+        "location": "home",
+        "code": 44
+    }]
+};
+
+var schema = {};
+
+function jsonCover(json, obj, array) {
+    if (!isEmpty(json)) {
+        for (var i in json) {
+            (function(name) {
+                var cnt = isType(json[name]);
+                obj[name] = {};
+                obj[name].type = cnt;
+
+                if (cnt === 'object') {
+                    obj[name].properties = {};
+                    obj[name].require = [];
+                    if (!isEmpty(json[name])) {
+                        jsonCover(json[name], obj[name].properties, obj[name].require);
+                    }
+                } else if (cnt === 'array') {
+                    obj[name].items = {};
+                    if (!isEmpty(json[name])) {
+                        obj[name].items.properties = {};
+                        obj[name].items.require = [];
+                        jsonCover(json[name][0], obj[name].items.properties, obj[name].items.require);
+                    }
+                } else {
+                    array.push(name);
+                }
+            }(i))
+        }
+    } else {
+        return json.length === +json.length ? [] : {};
     }
 }
 
+function isType(value) {
+    return Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
+}
 
-//判断 字段 对象 数组  三种类型
-//递归循环
+function isEmpty(value) {
+    return (Array.isArray(value) && value.length === 0) || (Object.prototype.isPrototypeOf(value) && Object.keys(value).length === 0);
+}
+
+jsonCover(json, schema, []);
+console.log(JSON.stringify(schema, null, 4));
